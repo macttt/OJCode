a0434eba218999ed3d603cd473d47f8612a2c5f7
macttt <macttt@163.com>
2019-04-09 23:02:33 +0800

    heap

--- src/DataStructsandAlgorithms/HeapByArray.java
+++ src/DataStructsandAlgorithms/HeapByArray.java
@@ -1,13 +1,23 @@
 package DataStructsandAlgorithms;
 
 
-/** 数组型堆的数据结构*/
+import java.util.Arrays;
+
+/**
+ *
+ *
+ *
+ * 大顶堆
+ * 数组型堆的数据结构*/
 public class HeapByArray {
     private int heapSize;
     private int[] heapArray;
     public HeapByArray(){
+        this.heapArray = new int[10];
+        Arrays.fill(heapArray,-1);
+        this.heapArray[1] = 1;
         this.heapSize = 0;
-        this.heapArray = new int[heapSize];
+        heapArray[0] = 1;
     }
 
     public HeapByArray(int size){
@@ -26,6 +36,7 @@ public class HeapByArray {
         heapArray[1] = rootValue;
     }
 
+    //让copy其他数组进堆，然后进行堆化或者排序
     public HeapByArray copyArray(int[] source){
         if(heapArray.length<=source.length){
             heapArray = new int[source.length+1];
@@ -35,7 +46,18 @@ public class HeapByArray {
         return this;
     }
 
+    //让数组成堆
     public void buildHeap(){
+        buildInClassHeap(1);
+    }
 
+    private void buildInClassHeap(int index){
+        if(index*2<heapArray.length-1&&heapArray[index*2]>=0) buildInClassHeap(index*2);
+        if(index*2+1<heapArray.length-1&&heapArray[index*2+1]>=0) buildInClassHeap(index*2+1);
+        if(heapArray[index]>heapArray[index/2]){
+            int tmp = heapArray[index/2];
+            heapArray[index/2] = heapArray[index];
+            heapArray[index] = tmp;
+        }
     }
 }
