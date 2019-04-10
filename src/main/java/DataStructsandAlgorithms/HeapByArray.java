7f74ed34a14c1df5a493a3df74aa2e41da5672e1
autobole <devbot@autobole.com>
2019-04-10 13:20:23 +0800

    import Maven

--- src/main/java/DataStructsandAlgorithms/HeapByArray.java
+++ src/main/java/DataStructsandAlgorithms/HeapByArray.java
@@ -1,15 +1,23 @@
 package DataStructsandAlgorithms;
 
 
+import Utils.CommonUtil;
+
 import java.util.Arrays;
 
+
 /**
- *
- *
  *
  * 大顶堆
  * 数组型堆的数据结构*/
 public class HeapByArray {
+    public static void main(String[] args){
+        int[] anArray = {5,3,1,6,1,3,6,8,6,4,3,4,9,10,24,13,11,14};
+        HeapByArray instance = new HeapByArray();
+        instance.copyArray(anArray);
+        instance.printHeapArray();
+    }
+
     private int heapSize;
     private int[] heapArray;
     public HeapByArray(){
@@ -36,13 +44,15 @@ public class HeapByArray {
         heapArray[1] = rootValue;
     }
 
+
     //让copy其他数组进堆，然后进行堆化或者排序
     public HeapByArray copyArray(int[] source){
         if(heapArray.length<=source.length){
             heapArray = new int[source.length+1];
         }
-
-
+        for(int i=0;i<source.length;i++) heapArray[i+1] = source[i];
+        heapArray[0] = source.length+1;
+        this.buildHeap();
         return this;
     }
 
@@ -51,6 +61,7 @@ public class HeapByArray {
         buildInClassHeap(1);
     }
 
+    //适应标准化接口的内部函数，按照下标造堆
     private void buildInClassHeap(int index){
         if(index*2<heapArray.length-1&&heapArray[index*2]>=0) buildInClassHeap(index*2);
         if(index*2+1<heapArray.length-1&&heapArray[index*2+1]>=0) buildInClassHeap(index*2+1);
@@ -59,5 +70,9 @@ public class HeapByArray {
             heapArray[index/2] = heapArray[index];
             heapArray[index] = tmp;
         }
+    }
+
+    public void printHeapArray(){
+        CommonUtil.printArray(this.heapArray);
     }
 }
---
+++ src/main/java/Utils/excelUtils.java
@@ -0,0 +1,39 @@
+package Utils;
+
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 从excel文件中读取数据
+ * */
+public class excelUtils {
+    public static Workbook getWorkBook(MultipartFile file) {
+        //获得文件名
+        String fileName = file.getOriginalFilename();
+        //创建Workbook工作薄对象，表示整个excel
+        Workbook workbook = null;
+        try {
+            //获取excel文件的io流
+            InputStream is = file.getInputStream();
+            //根据文件后缀名不同(xls和xlsx)获得不同的Workbook实现类对象
+            if (fileName.endsWith("xls")) {
+                //2003
+                workbook = new HSSFWorkbook(is);
+            } else if (fileName.endsWith("xlsx")) {
+                //2007
+                workbook = new XSSFWorkbook(is);
+            }
+        } catch (IOException e) {
+            //log.info e.getMessage()
+            e.printStackTrace();
+        }
+        return workbook;
+    }
+}
