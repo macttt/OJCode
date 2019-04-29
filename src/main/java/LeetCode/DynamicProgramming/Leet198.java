ce7b820d63cc7763192aaff840c740e34fe84520
autobole <devbot@autobole.com>
2019-04-29 18:28:13 +0800

    update

---
+++ src/main/java/LeetCode/DynamicProgramming/Leet198.java
@@ -0,0 +1,40 @@
+package LeetCode.DynamicProgramming;
+
+/**
+ * 198. House Robber
+ * Easy
+ *
+ * 2382
+ *
+ * 72
+ *
+ * Favorite
+ *
+ * Share
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ *              Total amount you can rob = 1 + 3 = 4.
+ * Example 2:
+ *
+ * Input: [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ *              Total amount you can rob = 2 + 9 + 1 = 12.
+ * */
+public class Leet198 {
+    /**
+     * 典型的01背包问题，在问题中加了限制
+     * */
+    public int rob(int[] nums) {
+        int maxRob = 0;
+
+        return maxRob;
+    }
+}
